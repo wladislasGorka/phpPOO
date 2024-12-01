41649c48e3b9faa378c631c979e1f3799f56f884
wladislasGorka <wladislas.gorka@gmail.com>
2024-12-01 10:34:34 +0100

    selectedMobs

--- gameInit.php
+++ gameInit.php
@@ -18,11 +18,30 @@ for( $i=1; $i<5; $i++){
     $talentName=$_POST["characterTalent$i"];
     $characters[] = new Character($_POST["characterName$i"],new $raceName(),new $talentName());
 }
-echo '<pre>'; print_r($characters); echo '</pre>';
-//Personnages stockés dans la session
+//echo '<pre>'; print_r($characters); echo '</pre>';
 
-//Instanciation des Mobs
+//Personnages stockés dans la session
+$_SESSION['characters']=$characters;
+
+//Instanciation des Mobs par tirage aléatoire
+//Tableau constant de toutes les créatures du jeu répartit selon leur niveau
+$mobs=[
+    ['Skeleton','Mimic'],
+    ['Fisherman','Apothecary'],
+    ['Golem','Gorgon'],
+    ['Lich','Hydra'],
+    ['Ferrovore','Kraken']
+];
+//Tirage aléatoire des Mobs pour le jeu en cours (1 créature de chaque niveau)
+$selectedMobs=[];
+for($i=0; $i< 5; $i++){
+    $selectedMobs[$i]=$mobs[$i][rand(0,count($mobs[$i])-1)];
+}
+//echo '<pre>'; print_r($selectedMobs); echo '</pre>';
 
 //Mobs stockés dans la session
+$_SESSION['mobs']=$selectedMobs;
 
-//redirection sur combat.php
+//Instanciation du combat
+//combat stocké dans la session
+//redirection sur battlefield.php
